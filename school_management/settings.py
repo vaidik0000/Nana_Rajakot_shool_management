47d5165d1af165128dd2091c14f66e5c4110facd
vaidik0000 <kotadiyavaidik34@gmail.com>
2025-05-05 12:06:00 +0530

    new change

--- school_management/settings.py
+++ school_management/settings.py
@@ -80,7 +80,10 @@ WSGI_APPLICATION = 'school_management.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
